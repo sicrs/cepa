2b71340767f12bc9b498a0ca897cc5e0d6a2c079
Sebastian <thesicrs@gmail.com>
2020-07-12 10:32:09 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<div align="center">
+	<h1><code>cepa</code></h1>
+</div>
+
+Basically some dumb powershell scripts to run a local [tor](https://ww.torproject.org) socks proxy lmao 
+
+# Usage
+1. Clone or download the repository and extract to a folder
+2. run `powershell .\cepa.ps1 -do help` in the same folder containing the Powershell script
---
+++ cepa.ps1
@@ -0,0 +1,89 @@
+param (
+		[string]$do
+	  )
+
+function Add-TorPath {
+	Set-Item -Path Env:Path -Value ($env:Path + (";" + $env:LOCALAPPDATA + "\tor\Tor"))
+}
+
+function Install-Tor {
+	if(Test-Path $env:LOCALAPPDATA\tor -PathType Container) {
+		Write-Output "Removing old dir"
+		Remove-Item -Path $env:LOCALAPPDATA\tor
+	}
+	try {
+		Write-Output "Downloading latest expert-bundle binary..."
+		Invoke-WebRequest -Uri "https://www.torproject.org/dist/torbrowser/9.5.1/tor-win32-0.4.3.5.zip" -Outfile "bin.zip"
+		Write-Output "Finished downloading."
+		Write-Output "Unzipping"
+		Expand-Archive -Path "bin.zip" -DestinationPath $env:LOCALAPPDATA\tor
+		Write-Output "Finished unzipping"
+		Remove-Item -Path bin.zip
+	}
+	catch {
+		Write-Error ("Oof: " + $Error)
+	}
+}
+
+function Start-Tor {
+	Write-Output "Starting tor..."
+	tor --service start 
+}
+
+function Stop-Tor {
+	Write-Output "Stopping tor..."
+	tor --service stop
+}
+
+function Register-Tor {
+	Write-Output "Registering tor as a service"
+	tor --service install --options -f $env:LOCALAPPDATA\tor\torrc --allow-missing-torrc
+}
+
+function Deregister-Tor {
+	Write-Verbose "Removing tor service"
+	tor --service remove
+	Write-Verbose "tor service removed."
+}
+
+if ($do -eq "") {
+	# default: check if binaries are found
+	if (Test-Path $env:LOCALAPPDATA\tor -PathType Container) {
+		# found, therefore run
+			
+	} else {
+		Install-Tor
+	}
+	Add-TorPath
+	Start-Tor
+} else {
+	Add-TorPath
+
+	if ($do -eq "start") {
+		Start-Tor
+	} elseif ($do -eq "stop") {
+		Stop-Tor
+	} elseif ($do -eq "register") {
+		Register-Tor
+	} elseif ($do -eq "deregister") {
+		Deregister-Tor
+	} elseif ($do -eq "hard-start") {
+		tor -f $env:LOCALAPPDATA\tor\torrc --allow-missing-torrc
+	} elseif ($do -eq "install") {
+		Install-Tor
+	} elseif ($do -eq "help") {
+		Write-Output "
+Syntax: powershell .\cepa.ps1 [-do <action>]
+action:
+	start: start tor as a service
+	stop: stop tor (service)
+	hard-start: start tor in the same command window
+	register: register tor as a service
+	deregister: deregister tor as a service
+	install: install
+	help: show this message
+		"
+	} else {
+		Write-Error ("Invalid action: " + $do)
+	}
+}
